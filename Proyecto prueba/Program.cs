5969cb8647e32edbb4a6c020d89a0154097ee8a9
SamuelGarciaBazan <samuelffxii@outlook.com>
2023-03-09 12:23:34 +0100

    primer commit

---
+++ Proyecto prueba/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
