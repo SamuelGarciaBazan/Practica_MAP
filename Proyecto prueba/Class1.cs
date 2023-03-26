e06eb72ae75ded395a1b3de5b7b6bed4f8c3ff32
Luis Enrique Cabello Blanco <luisecab@ucm.es>
2023-03-26 16:49:50 +0200

    clase Vector

---
+++ Proyecto prueba/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_prueba
+{
+    internal class Vector
+    {
+        int x;
+        int y;
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+
+        public double Modulo()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public double Inclinacion()
+        {
+            return Math.Tan(y / x);
+        }
+    }
+}
